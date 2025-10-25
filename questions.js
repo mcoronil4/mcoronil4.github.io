1c8532e3917cf5aed9fd90acc52ed8855eaaed09
mcoronil4 <mcoronil4@iespompeufabra.cat>
2025-10-25 12:27:04 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -279,6 +279,54 @@ const questions = [
             <br><br>
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
         images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
+    },
+    {
+        type: "questions", // Tipus de pregunta
+        category: "pneumatics_hydraulics",
+        text: `El Ti-6Al-7Nb és un aliatge biocompatible de titani àmpliament utilitzat en aplicacions quirúrgiques. Conté un 5,5 % d’alumini (Al), un 6,5 % de niobi (Nb), 0,25 % de ferro (Fe), un 0,08 % de carboni (C) i la resta és titani (Ti). En l’obtenció d’aquest aliatge, quina quantitat de niobi cal per a aliar-lo amb 250 kg de titani?`,
+        options: [
+            { text: "353,1 kN", value: "a" },
+            { text: "392,3 N", value: "b" },
+            { text: "40 N", value: "c" },
+            { text: "36 kN", value: "d" }
+        ],
+        correctAnswer: "b",
+        steps: `
+            <strong>Càlcul de la força sobre l'èmbol petit:</strong><br>
+            Pressió hidràulica:<br>
+            \\[
+            F_1 = F_2 \\cdot \\frac{A_1}{A_2}
+            \\]<br>
+            Massa a aixecar:<br>
+            \\[
+            m = 1200\\,\\text{kg}
+            \\]<br>
+            Gravetat:<br>
+            \\[
+            g = 9,807\\,\\text{m·s²}
+            \\]<br>
+            Força sobre l'èmbol gran:<br>
+            \\[
+            F_2 = m \\cdot g = 1200\\,\\text{kg} \\times 9,807\\,\\text{m·s²} = 11768,4\\,\\text{N}
+            \\]<br>
+            Àrea de l'èmbol petit:<br>
+            \\[
+            A_1 = 1000\\,\\text{mm²} = 1000 \\times 10⁻⁶\\,\\text{m²} = 0,001\\,\\text{m²}
+            \\]<br>
+            Àrea de l'èmbol gran:<br>
+            \\[
+            A_2 = 30000\\,\\text{mm²} = 30000 \\times 10⁻⁶\\,\\text{m²} = 0,03\\,\\text{m²}
+            \\]<br>
+            Força sobre l'èmbol petit:<br>
+            \\[
+            F_1 = F_2 \\cdot \\frac{A_1}{A_2} = 11768,4\\,\\text{N} \\cdot \\frac{0,001\\,\\text{m²}}{0,03\\,\\text{m²}}
+            \\]<br>
+            \\[
+            F_1 = 392,28\\,\\text{N}
+            \\]
+            <br><br>
+            <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
+        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
     }
 ];
 
