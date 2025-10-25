b571523edc993d02b43e60975b63d73f33c01278
mcoronil4 <mcoronil4@iespompeufabra.cat>
2025-10-25 12:04:49 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -235,7 +235,7 @@ const questions = [
     {
         type: "questions", // Tipus de pregunta
         category: "motors_reductors",
-        text: `Una porta corredissa automàtica és accionada mitjançant un motor que gira a \\(n_{\\text{mot}} = 1\,500\\,\\text{min}^{-1}\\) i un reductor. L’eix de sortida del reductor és un pinyó de radi primitiu \\(r = 30\\,\\text{mm}\\) que acciona una cremallera fixa a la porta. En règim nominal, la porta es trasllada a una velocitat constant \\(v_{\\text{nom}} = 0,12\\,\\text{m/s}\\). Determineu la relació de transmissió entre la velocitat angular de l’eix de sortida del reductor i la velocitat angular de l’eix del motor: \\(\\tau = \\frac{\\omega_{\\text{red}}}{\\omega_{\\text{mot}}}\\).`,
+        text: `Es vol aixecar una massa \\(m = 1\,200\\,\\text{kg}\\) utilitzant una premsa hidràulica. La secció transversal de l’èmbol gran és \\(A_{gran} = 30\,000\\,\\text{mm}^2\\) i la del petit és \\(A_{petit} = 1\,000\\,\\text{mm}^2\\). Preneu \\(g = 9,807\\,\\text{m/s}^2\\). Determineu la força \\(F\\) que cal exercir sobre l’èmbol petit: \\(F = ?\\).`,
         options: [
             { text: "39,27", value: "a" },
             { text: "0,7639 × 10^{-3}", value: "b" },
