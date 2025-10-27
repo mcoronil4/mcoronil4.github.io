8137a007613a77f726c41bd02d45952cebf8bbd8
mcoronil4 <mcoronil4@iespompeufabra.cat>
2025-10-27 12:19:45 +0100

    Update questions.js

--- questions.js
+++ questions.js
@@ -257,7 +257,7 @@ const questions = [
             \\]
             <br><br>
             <img src="ruta/a/imatgeResposta.jpg" alt="Imatge relacionada amb la resposta">`,  // Imatge opcional al final de la resposta
-        images: `<img src="ruta/a/imatgePregunta.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
+        images: `<img src="images/2020junyserie1q1.jpg" alt="Imatge relacionada amb la pregunta">`  // Imatge opcional al final de la pregunta
     },
     {
         type: "questions", // Tipus de pregunta
